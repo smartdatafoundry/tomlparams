a2a491e2a1fb86aa46a9cd1ddafed8cf6d499882
Nick Radcliffe <njr@StochasticSolutions.com>
2023-06-22 11:28:44 +0100

    console working

--- tomlparams/console.py
+++ tomlparams/console.py
@@ -1,24 +1,19 @@
 import os
 import shutil
 import sys
+import unittest
 
 from tomlparams import __version__
+from tomlparams.tests.test_tomlparams import TestTOMLParams
 
 
 
 DIR = os.path.dirname(os.path.realpath(os.path.abspath(__file__)))
 PARENT_DIR = os.path.dirname(DIR)
 EXAMPLES_DIR = os.path.join(PARENT_DIR, 'examples')
-TEST_DIR = os.path.join(PARENT_DIR, 'tests')
-print(f'DIR: {DIR}')
-print(f'PARENT_DIR: {PARENT_DIR}')
-print(f'TEST_DIR: {TEST_DIR}')
-
-sys.path.append(TEST_DIR)
-from test_tomlparams import TestTOMLParams
 
 
-HELP = '''TOMLParams
+USAGE = '''TOMLParams
 
 USAGE:
     tomlparams help      --- show this message
@@ -26,8 +21,9 @@ USAGE:
     tomlparams examples  --- copy the examples to ./tomlparams_examples
     tomlparams test      --- run the tomlparams tests
 
-Source: https://github.com/smartdatafoundry.com/tomlparams
 Documentation: https://tomlparams.readthedocs.io/
+Source code:   https://github.com/smartdatafoundry.com/tomlparams
+Website:       https://tomlparams.com
 
 Installation:
 
@@ -37,22 +33,31 @@ Installation:
 
 
 def main(args):
-    if len(args) < 1:
-        print(HELP)
-        sys.argv(0)
-    cmd = args[1].lower()
-    if cmd in ('help', '--help', '-h'):
-        print(HELP)
-    elif cmd in ('version', '--version', '-v'):
-        print(__version__)
-    elif cmd == 'examples':
-        dest_dir = os.path.abspath('.')
-        dest_path = os.path.join(dest_dir, 'tomlparams_examples')
-        shutil.copytree(EXAMPLES_DIR, dest_path)
-        print(f'Examples copied to {dest_path}.')
-    elif cmd == 'test':
-        from tdda.referencetest import ReferenceTestCase
-        ReferenceTestCase.main()
+    if len(args) < 2:
+        print(USAGE)
+    else:
+        cmd = args[1].lower()
+        if cmd in ('help', '--help', '-h'):
+            print(USAGE)
+        elif cmd in ('version', '--version', '-v'):
+            print(__version__)
+        elif cmd == 'examples':
+            dest_dir = os.path.abspath('.')
+            dest_path = os.path.join(dest_dir, 'tomlparams_examples')
+            shutil.copytree(EXAMPLES_DIR, dest_path)
+            print(f'Examples copied to {dest_path}.')
+        elif cmd == 'test':
+            suite = unittest.TestSuite()
+            testloader = unittest.TestLoader()
+            s = testloader.loadTestsFromTestCase(TestTOMLParams)
+            suite.addTest(s)
+            tester = unittest.TextTestRunner()
+            tester.run(suite)
+
+        else:
+            print(f'*** Unknown command: {" ".join(args)}\n')
+            print(USAGE, file=sys.stderr)
+            sys.exit(1)
 
 
 if __name__ == '__main__':
