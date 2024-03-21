577408b501ba9042c61e853bb878c11bac273165
vdiaz <vadrienzo@gmail.com>
2024-03-21 15:11:43 +0000

    Changing concatenate_keys

--- tomlparams/utils.py
+++ tomlparams/utils.py
@@ -39,24 +39,35 @@ def load_toml(path):
 
 
 def concatenate_keys(
-    dict_to_flatten: dict[Any, Any], sep: str = '.'
+    d: dict, sep='.'
 ) -> Generator[tuple[str, Any], None, None]:
     """
     Concatenate keys in a nested dict, e.g.:
             >>> d = {'a': {'b': 1, 'c': 2}, 'd': 3}
             >>> dict(concat_keys(d))
             {'a.b': 1, 'a.c': 2, 'd': 3}
+    Special when values is a list:
+            >>> d = {'a': {'b': 1, 'c': 2}, 'd': [3, {'e': 4}]}
+            >>> dict(concat_keys(d))
+            {'a.b': 1, 'a.c': 2, 'd.0': 3, 'd.1.e': 4}
     Args:
         d: dict
         sep: separator between keys
     Returns:
         generator of (key, value) pairs
     """
-    for key1, value1 in dict_to_flatten.items():
+    for key1, value1 in d.items():
         if isinstance(value1, dict):
             for key2, value2 in dict(
                 concatenate_keys(value1, sep=sep)
             ).items():
                 yield key1 + sep + key2, value2
+        elif isinstance(value1, list):
+            for list_index, list_item in enumerate(value1):
+                if isinstance(list_item, dict):
+                    for key3, value3 in concatenate_keys(list_item, sep=sep):
+                        yield f'{key1}{sep}{list_index}{sep}{key3}', value3
+                else:
+                    yield f'{key1}{sep}{list_index}', value2
         else:
             yield key1, value1
