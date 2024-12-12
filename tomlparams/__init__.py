9ba29fc2260a5e2fc6b71084bfc464f52696ec5f
githubactions <githubactions@github.com>
2024-12-12 16:04:23 +0000

    bump version

--- tomlparams/__init__.py
+++ tomlparams/__init__.py
@@ -1,5 +1,5 @@
-__version__ = '0.1.26'
+__version__ = '0.1.27'
 from tomlparams.params_group import ParamsGroup
 from tomlparams.tomlparams import TOMLParams
 
-__all__ = ['__version__', 'ParamsGroup', 'TOMLParams']
+__version__ = '0.1.27'
